dd3100534f2cd02fce0062f6befa4717f3a47720
root <root@LAPTOP-GBV8SAPB.localdomain>
2020-04-13 05:52:49 -0500

    ‘AddedvalueforSUM’

--- AddNum.java
+++ AddNum.java
@@ -2,7 +2,7 @@
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
+      int num1 = 5, num2 = 15, sum=0;
       sum = num1 + num2;
 
       System.out.println("Sum of these numbers: "+sum);
